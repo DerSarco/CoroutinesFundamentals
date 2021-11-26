1fd12b03cb5523e86ad212d9ef8484c2d4b9ab9b
Carlos Muñoz Bustamante <carlos.a.munoz2@outlook.com>
2021-11-26 09:25:43 -0300

    Example of consumer-productor pattern, very basic.

--- app/src/main/java/com/sarco/fundamentoscorrutinas/CoroutinesBasic.kt
+++ app/src/main/java/com/sarco/fundamentoscorrutinas/CoroutinesBasic.kt
@@ -1,6 +1,9 @@
 package com.sarco.fundamentoscorrutinas
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
 
 fun main() {
 //    globalScope()w
@@ -10,9 +13,71 @@ fun main() {
     newTopic("Constructores de corrutinas")
 //    cRunBlocking(
 //    cLaunch()
-    cAsync()
+//    cAsync()
+//    job()
+//    deferred()
+    cProduce()
+
 //    esta linea espera que reciba un carácter desde consola.
     readLine()
+}
+
+fun cProduce()  = runBlocking {
+    newTopic("Produce")
+    val names = produceNames()
+    names.consumeEach { println(it)}
+}
+
+fun CoroutineScope.produceNames(): ReceiveChannel<String> = produce {
+    (1..5).forEach{ send("name$it")}
+}
+
+fun deferred() {
+    runBlocking {
+        newTopic("Deferred")
+
+        val deferred = async {
+            startMsg()
+            delay(someTime())
+            println("Deferred...")
+            endMsg()
+            multi(5,2)
+        }
+
+        println("Deferred: $deferred")
+        println("Valor del Deferred.await: ${deferred.await()}")
+
+
+        val result = async {
+            multi(3,3)
+        }.await()
+
+        println(result)
+    }
+}
+
+fun job() {
+    runBlocking {
+        newTopic("Job")
+        val job = launch {
+            startMsg()
+            delay(2_100)
+            println("Job executed")
+            endMsg()
+        }
+        println("Job: $job")
+        println("isActive: ${job.isActive}")
+        println("isCancelled: ${job.isCancelled}")
+        println("isCompleted: ${job.isCompleted}")
+
+        delay(someTime())
+        println("Tarea cancelada")
+        job.cancel()
+
+        println("isActive: ${job.isActive}")
+        println("isCancelled: ${job.isCancelled}")
+        println("isCompleted: ${job.isCompleted}")
+    }
 }
 
 fun cAsync() {
