6edc4bb5f67e8bed072e2d8d1626b147f853a96a
Carlos Muñoz Bustamante <carlos.a.munoz2@outlook.com>
2021-11-26 14:36:08 -0300

    Flows examples, intermediate operators, Map, Filter, Transform & take
    Terminal Flow Operators, List, Single, First, Last, Reduce, Fold

--- app/src/main/java/com/sarco/fundamentoscorrutinas/Flows.kt
+++ app/src/main/java/com/sarco/fundamentoscorrutinas/Flows.kt
@@ -1,8 +1,7 @@
 package com.sarco.fundamentoscorrutinas
 
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.util.*
@@ -10,8 +9,60 @@ import java.util.*
 fun main() {
 //    coldFLow()
 //    cancelFLow()
-    flowOperators()
+//    flowOperators()
+    terminalFlowOperators()
 }
+
+fun terminalFlowOperators() {
+    runBlocking {
+        newTopic("Operadores Flow terminales")
+        newTopic("List")
+
+        val list = getDataByFlow()
+//            .toList()
+        println("List: $list")
+
+        newTopic("Single")
+
+        val single = getDataByFlow()
+//            .take(1)
+//            .single()
+        println("Single: $single")
+
+        newTopic("First")
+        val first = getDataByFlow()
+//            .first()
+        println("First: $first")
+
+        newTopic("Last")
+        val last = getDataByFlow()
+//            .last()
+        println("Last: $last")
+
+        newTopic("Reduce")
+        val saving = getDataByFlow()
+            .reduce { accumulator, value ->
+            println("Acumulator: $accumulator")
+            println("Value: $value")
+            println("Current saving = ${accumulator + value} ")
+            accumulator + value
+        }
+        println("Saving: $saving")
+
+        newTopic("Fold")
+
+        val lastSaving = saving
+        val totalSaving = getDataByFlow()
+            .fold(lastSaving) {acc, value ->
+                println("Acumulator: $acc")
+                println("Value: $value")
+                println("Current saving = ${acc + value} ")
+                acc + value
+            }
+        println("Total Saving: $totalSaving")
+    }
+}
+
 
 fun flowOperators() {
     runBlocking {
@@ -19,11 +70,36 @@ fun flowOperators() {
         newTopic("Map")
         getDataByFlow().map {
             setFormat(it)
-            setFormat(convertCelsToFahr(it), "F" )
-        }.collect {
-            println(it)
+            setFormat(convertCelsToFahr(it), "F")
         }
+//        }.collect {
+//            println(it)
+//        }
+        newTopic("Filter")
+        getDataByFlow().filter {
+            it < 23
+        }.map {
+            setFormat(it)
+        }
+//        .collect {
+//            println(it)
+//        }
 
+        newTopic("Transform")
+        getDataByFlow()
+            .transform {
+                emit(setFormat(it))
+                emit(setFormat(convertCelsToFahr(it), "F"))
+            }
+//            .collect { println(it) }
+
+        newTopic("Take")
+        getDataByFlow()
+            .take(3)
+            .map {
+                setFormat(it)
+            }
+            .collect { println(it) }
     }
 }
 
