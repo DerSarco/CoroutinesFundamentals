522ed218a30214f5a0570b1af1f4dd58c2480161
Carlos Muñoz Bustamante <carlos.a.munoz2@outlook.com>
2021-11-29 12:06:08 -0300

    Channel, example how to open a channel and how to send and receive data.

--- app/src/main/java/com/sarco/fundamentoscorrutinas/CoroutinesAdvanced.kt
+++ app/src/main/java/com/sarco/fundamentoscorrutinas/CoroutinesAdvanced.kt
@@ -1,9 +1,34 @@
 package com.sarco.fundamentoscorrutinas
 
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+val countries = listOf("Santander", "CDMX", "Lima", "Buenos Aires", "Santiago")
+
 fun main() {
     basicChannel()
 }
 
 fun basicChannel() {
+    runBlocking {
+        newTopic("Canal Básico")
+
+        val channel = Channel<String>()
+        launch {
+            countries.forEach{
+                channel.send(it)
+            }
+        }
+/*
+        repeat(5){
+            println(channel.receive())
+        }*/
+
+        for (value in channel){
+            println(value)
+        }
+
+    }
 
 }
