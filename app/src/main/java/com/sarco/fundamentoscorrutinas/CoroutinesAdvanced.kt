4cd4073c4bbac835d3f70b9d8b63ddc2638df230
Carlos Muñoz Bustamante <carlos.a.munoz2@outlook.com>
2021-11-29 12:15:51 -0300

    Channel, example how to close a channel and receive data.

--- app/src/main/java/com/sarco/fundamentoscorrutinas/CoroutinesAdvanced.kt
+++ app/src/main/java/com/sarco/fundamentoscorrutinas/CoroutinesAdvanced.kt
@@ -1,14 +1,51 @@
 package com.sarco.fundamentoscorrutinas
 
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 val countries = listOf("Santander", "CDMX", "Lima", "Buenos Aires", "Santiago")
 
 fun main() {
-    basicChannel()
+//    basicChannel()
+    closeChannel()
+
+
 }
+
+fun closeChannel() {
+    runBlocking {
+        newTopic("Cerrar un canal")
+
+        val channel = Channel<String>()
+
+        launch {
+            countries.forEach{
+                channel.send(it)
+                if(it == "Lima"){
+                    channel.close()
+                    return@launch
+                }
+            }
+//            channel.close()
+        }
+
+//        for (value in channel){
+//            println(value)
+//        }\
+
+        while (!channel.isClosedForReceive){
+            println(channel.receive())
+        }
+
+//        channel.consumeEach {
+//            println(it)
+//        }
+
+    }
+}
+
 
 fun basicChannel() {
     runBlocking {
